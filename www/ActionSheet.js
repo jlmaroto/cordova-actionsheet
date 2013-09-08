1bf3769cafeb617e519286a6437a77fb95f14b07
Etienne Ad <aze>
2013-09-07 20:20:20 -0400

    update js

--- www/ActionSheet.js
+++ www/ActionSheet.js
@@ -2,41 +2,37 @@
 //  ActionSheet.js
 //
 // Created by Olivier Louvignes on 2011-11-27.
+// Updated by Etienne Adriaenssen on 2013-09-07
 //
 // Copyright 2011-2012 Olivier Louvignes. All rights reserved.
 // MIT Licensed
+var exec = require('cordova/exec');
 
-(function(cordova) {
+function ActionSheet() {}
 
-	function ActionSheet() {}
+ActionSheet.prototype.create = function(options, callback) {
+	options || (options = {});
+	var scope = options.scope || null;
 
-	ActionSheet.prototype.create = function(options, callback) {
-		options || (options = {});
-		var scope = options.scope || null;
-
-		var config = {
-			title : options.title || '',
-			items : options.items || ['Cancel'],
-			style : options.style || 'default',
-			destructiveButtonIndex : options.hasOwnProperty('destructiveButtonIndex') ? options.destructiveButtonIndex : undefined,
-			cancelButtonIndex : options.hasOwnProperty('cancelButtonIndex') ? options.cancelButtonIndex : undefined
-		};
-
-		var _callback = function(result) {
-			var buttonValue = false, // value for cancelButton
-				buttonIndex = result.buttonIndex;
-			if(!config.cancelButtonIndex || buttonIndex != config.cancelButtonIndex) {
-				buttonValue = config.items[buttonIndex];
-			}
-			if(typeof callback == 'function') callback.apply(scope, [buttonValue, buttonIndex]);
-		};
-
-		return cordova.exec(_callback, _callback, 'ActionSheet', 'create', [config]);
+	var config = {
+		title : options.title || '',
+		items : options.items || ['Cancel'],
+		style : options.style || 'default',
+		destructiveButtonIndex : options.hasOwnProperty('destructiveButtonIndex') ? options.destructiveButtonIndex : undefined,
+		cancelButtonIndex : options.hasOwnProperty('cancelButtonIndex') ? options.cancelButtonIndex : undefined
 	};
 
-	cordova.addConstructor(function() {
-		if(!window.plugins) window.plugins = {};
-		window.plugins.actionSheet = new ActionSheet();
-	});
+	var _callback = function(result) {
+		var buttonValue = false, // value for cancelButton
+		buttonIndex = result.buttonIndex;
+		if(!config.cancelButtonIndex || buttonIndex != config.cancelButtonIndex) {
+			buttonValue = config.items[buttonIndex];
+		}
+		if(typeof callback == 'function') callback.apply(scope, [buttonValue, buttonIndex]);
+	};
 
-})(window.cordova || window.Cordova);
+	return cordova.exec(_callback, _callback, 'ActionSheet', 'create', [config]);
+};
+
+module.exports = new ActionSheet();
+
