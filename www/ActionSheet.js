3aaaafd36827f18047e76040f009f687793c7622
Etienne Ad <aze>
2013-09-27 10:34:21 -0400

    tests for android

--- www/ActionSheet.js
+++ www/ActionSheet.js
@@ -1,4 +1,4 @@
-//
+
 //  ActionSheet.js
 //
 // Created by Olivier Louvignes on 2011-11-27.
@@ -8,31 +8,95 @@
 // MIT Licensed
 var exec = require('cordova/exec');
 
-function ActionSheet() {}
+var ActionSheet = function() {}
 
 ActionSheet.prototype.create = function(options, callback) {
-	options || (options = {});
-	var scope = options.scope || null;
+	
+	if(/android/i.test(navigator.userAgent)){
+		options || (options = {});
+		var scope = options.scope || null;
 
-	var config = {
-		title : options.title || '',
-		items : options.items || ['Cancel'],
-		style : options.style || 'default',
-		destructiveButtonIndex : options.hasOwnProperty('destructiveButtonIndex') ? options.destructiveButtonIndex : undefined,
-		cancelButtonIndex : options.hasOwnProperty('cancelButtonIndex') ? options.cancelButtonIndex : undefined
-	};
+		var config = {
+			title : options.title || '',
+			items : options.items || ['Cancel'],
+			style : options.style || 'default',
+			destructiveButtonIndex : options.hasOwnProperty('destructiveButtonIndex') ? options.destructiveButtonIndex : undefined,
+			cancelButtonIndex : options.hasOwnProperty('cancelButtonIndex') ? options.cancelButtonIndex : undefined
+		};
 
-	var _callback = function(result) {
-		var buttonValue = false, // value for cancelButton
-		buttonIndex = result.buttonIndex;
-		if(!config.cancelButtonIndex || buttonIndex != config.cancelButtonIndex) {
-			buttonValue = config.items[buttonIndex];
+		var _callback = function(result) {
+			if(!result)
+				return;
+			var buttonValue = false, 
+			buttonIndex = result.buttonIndex;
+			if(!config.cancelButtonIndex || buttonIndex != config.cancelButtonIndex) {
+				buttonValue = config.items[buttonIndex];
+			}
+			console.log(buttonIndex);
+			if(typeof callback == 'function') callback.apply(scope, [buttonValue, buttonIndex]);
+			return buttonIndex;
+		};
+
+		$('#ActionSheet').empty();
+		var html = '';
+		for (var i = config.items.length - 1; i >= 0; i--) {
+			if(i==config.cancelButtonIndex){
+				html='<span class="button-inset actionsheetbtn" id="test'+i+'" style="position:relative;width: 100%;z-index:15000;line-height:45px">'
+				+'<font color="red">'+config.items[i]+'</font>'+'</span>'+html;
+			}else{
+				html='<span class="button-inset actionsheetbtn" id="test'+i+'" style="position:relative;width: 100%;z-index:15000;line-height:45px">'
+				+config.items[i]+'</span>'+html;
+			}
 		}
-		if(typeof callback == 'function') callback.apply(scope, [buttonValue, buttonIndex]);
+
+		html = '<div id="alterbarquestion" class="alertbar" style="position:absolute;z-index:14999;width:100%"><font>'+config.title+'</font>'+html+'</div>';
+		$('.actionsheet-btn').off('touchstart');
+		$('#ActionSheet').html(html);
+
+		setTimeout(function(){
+			$('.actionsheetbtn').on('touchstart', function(e){
+				e.preventDefault();
+				e.stopPropagation();
+				var id = $(e.currentTarget).attr('id');
+				id = id.replace('test', '');
+				var result = {};
+				result.buttonIndex = id;
+				result.buttonValue = $(e.currentTarget).text();
+				if(id == config.cancelButtonIndex)
+					$('#ActionSheet').hide();
+
+				return _callback(result);
+
+			});
+		},10);
+
+		$('#ActionSheet').show();
+		return;
+
+
+	}else{
+
+		options || (options = {});
+		var scope = options.scope || null;
+
+		var config = {
+			title : options.title || '',
+			items : options.items || ['Cancel'],
+			style : options.style || 'default',
+			destructiveButtonIndex : options.hasOwnProperty('destructiveButtonIndex') ? options.destructiveButtonIndex : undefined,
+			cancelButtonIndex : options.hasOwnProperty('cancelButtonIndex') ? options.cancelButtonIndex : undefined
+		};
+
+		var _callback = function(result) {
+			var buttonValue = false,
+			buttonIndex = result.buttonIndex;
+			if(!config.cancelButtonIndex || buttonIndex != config.cancelButtonIndex) {
+				buttonValue = config.items[buttonIndex];
+			}
+			if(typeof callback == 'function') callback.apply(scope, [buttonValue, buttonIndex]);
+		};
+
+		return cordova.exec(_callback, _callback, 'ActionSheet', 'create', [config]);
 	};
-
-	return cordova.exec(_callback, _callback, 'ActionSheet', 'create', [config]);
-};
-
+}
 module.exports = new ActionSheet();
-
