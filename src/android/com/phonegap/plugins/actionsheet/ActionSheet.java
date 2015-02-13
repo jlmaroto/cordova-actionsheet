0336ebed8abe255f010e99fdb756b28d6d9f1c05
Jose Luis Maroto <maroto.joseluis@gmail.com>
2015-02-13 04:43:08 +0100

    a

--- src/android/com/phonegap/plugins/actionsheet/ActionSheet.java
+++ src/android/com/phonegap/plugins/actionsheet/ActionSheet.java
@@ -117,7 +117,10 @@ public class ActionSheet extends CordovaPlugin {
             // java 0 based index converted to cordova 1 based
             // index, so we don't confuse the webbies.
             JSONObject json = new JSONObject();
-            json.put("buttonIndex", which);
+            try {
+              json.put("buttonIndex", which);
+            } catch (JSONException e) {
+            }
 
             callbackContext.sendPluginResult(new PluginResult(
                 PluginResult.Status.OK, json));
